8bbaa9af874f5cbca9c4539e74f87a598c7e8dc1
MOONLAPSED <137737122+MOONLAPSED@users.noreply.github.com>
2025-11-23 21:19:39 -0600

    jupyter server removed; put it back in later

--- .devcontainer/provisioning.sh
+++ .devcontainer/provisioning.sh
@@ -3,13 +3,6 @@ FILE=$1
 
 echo "Initializing..."
 
-# connectivity check
-if curl -fsSL https://example.com >/dev/null; then
-  echo "✅ internet OK"
-else
-  echo "❌ internet down"
-fi
-
 # "portsAttributes": {
 #   "8000": { "label": "App Server", "onAutoForward": "openBrowser" },
 #   "8888": { "label": "Jupyter", "onAutoForward": "notify" }
